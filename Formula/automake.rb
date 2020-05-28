548487a60f55f62fa67d53ed43bc16f67987702d
ppcbrew <macuser@localhost>
2020-05-28 17:03:56 -0500

    adding leopard_g5 bottle of automake

---
+++ Formula/automake.rb
@@ -0,0 +1,40 @@
+class Automake < Formula
+  revision 100
+  desc "Tool for generating GNU Standards-compliant Makefiles"
+  homepage "https://www.gnu.org/software/automake/"
+  url "http://ftpmirror.gnu.org/automake/automake-1.15.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/automake/automake-1.15.tar.xz"
+  sha256 "9908c75aabd49d13661d6dcb1bc382252d22cc77bf733a2d55e87f2aa2db8636"
+
+  bottle do
+    cellar :any
+    revision 2
+    sha256 "70a763221d2bb9baaf630f2170224c915ca96a9966fbb1d86781c8743740bb7b" => :el_capitan
+    sha256 "d8e4773130e25ff576a0c7d18b4d010b1e03eba90b0074e1ac749fdf3bc13e26" => :yosemite
+    sha256 "69c1635672fa682a40949572e64fe3495055e97ad2c105dd46fb2e447d0d65a8" => :mavericks
+    sha256 "6e6fdaa7fb7ddaaeb103341d1ca351e0669874f86eb21eb6623cb345dd1f5b6f" => :mountain_lion
+    sha1 "465d5d391aaac29f48c6ace503b06723d3269536" => :leopard_g5
+  end
+
+  depends_on "autoconf" => :run
+
+  keg_only :provided_until_xcode43
+
+  def install
+    ENV["PERL"] = "/usr/bin/perl"
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+
+    # Our aclocal must go first. See:
+    # https://github.com/Homebrew/homebrew/issues/10618
+    (share/"aclocal/dirlist").write <<-EOS.undent
+      #{HOMEBREW_PREFIX}/share/aclocal
+      /usr/share/aclocal
+    EOS
+  end
+
+  test do
+    system "#{bin}/automake", "--version"
+  end
+end
