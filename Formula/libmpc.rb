8d261f693eb737721115e3247cff9d0389c20ac3
ppcbrew <macuser@ibookg3>
2020-05-29 00:55:53 -0500

    adding tiger_g3 bottle of libmpc

--- Formula/libmpc.rb
+++ Formula/libmpc.rb
@@ -11,6 +11,7 @@ class Libmpc < Formula
     root_url "https://f002.backblazeb2.com/file/bottles"
     sha1 "5142a88e1fcca662827abb425e08987776a39fdd" => :leopard_g5
     sha1 "d9f8b7391584e5391860700f7b8e64bf540ff544" => :tiger_g4e
+    sha1 "2743e39c0f33d52aa2c7d98a0297481d1d2744a4" => :tiger_g3
   end
 
   depends_on "ppcbrew/tiger/gmp"
