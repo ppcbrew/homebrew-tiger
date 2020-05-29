4e4adfc502bc7b2de84f7c69718c3e24e7714e11
ppcbrew <macuser@pbookg4>
2020-05-29 02:17:07 -0500

    adding tiger_g4e bottle of lzop 1.04

--- Formula/lzop.rb
+++ Formula/lzop.rb
@@ -2,15 +2,13 @@ class Lzop < Formula
   revision 100
   desc "File compressor"
   homepage "http://www.lzop.org/"
-  url "http://www.lzop.org/download/lzop-1.03.tar.gz"
-  sha256 "c1425b8c77d49f5a679d5a126c90ea6ad99585a55e335a613cae59e909dbb2c9"
+  url "http://www.lzop.org/download/lzop-1.04.tar.gz"
+  sha1 "3540761ce8fc6dc42c326a9fcb1471e190a4db62"
 
   bottle do
     cellar :any
     root_url "https://f002.backblazeb2.com/file/bottles"
-    sha1 "94f89f47047a1a1b3ba7f26d6b973f22596e6ca6" => :leopard_g5
-    sha1 "8c6e99632e4edf599d9e7564a2a5806c504dcba6" => :tiger_g4e
-    sha1 "ff427a1ef5e442bf34a2da0ff0bd66daec7751e5" => :tiger_g3
+    sha1 "0e4f2108ecc5a19638442ac8b3bc18781e297e42" => :tiger_g4e
   end
 
   depends_on "ppcbrew/tiger/lzo"
