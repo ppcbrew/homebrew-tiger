2341387508462620fefe6282c9688e84a58f2907
ppcbrew <macuser@imacg5>
2020-05-29 12:49:40 -0500

    adding leopard_g5 bottle of sqlite 3.31.1

---
+++ Formula/sqlite.rb
@@ -0,0 +1,57 @@
+class Sqlite < Formula
+  revision 100
+  desc "Command-line interface for SQLite"
+  homepage "https://sqlite.org/"
+  url "https://sqlite.org/2020/sqlite-autoconf-3310100.tar.gz"
+  version "3.31.1"
+  sha256 "62284efebc05a76f909c580ffa5c008a7d22a1287285d68b7825a2b6b51949ae"
+
+  bottle do
+    cellar :any
+    root_url "https://f002.backblazeb2.com/file/bottles"
+    sha1 "c8a03740b4c2e39c4e4337778d7e40e07453987e" => :leopard_g5
+  end
+
+  #keg_only :provided_by_macos
+  keg_only :provided_by_osx
+
+  depends_on "ppcbrew/tiger/readline"
+
+  #uses_from_macos "zlib"
+
+  def install
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_COLUMN_METADATA=1"
+    # Default value of MAX_VARIABLE_NUMBER is 999 which is too low for many
+    # applications. Set to 250000 (Same value used in Debian and Ubuntu).
+    ENV.append "CPPFLAGS", "-DSQLITE_MAX_VARIABLE_NUMBER=250000"
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_RTREE=1"
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_FTS3=1 -DSQLITE_ENABLE_FTS3_PARENTHESIS=1"
+    ENV.append "CPPFLAGS", "-DSQLITE_ENABLE_JSON1=1"
+
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --enable-dynamic-extensions
+      --enable-readline
+      --disable-editline
+      --enable-session
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    path = testpath/"school.sql"
+    path.write <<~EOS
+      create table students (name text, age integer);
+      insert into students (name, age) values ('Bob', 14);
+      insert into students (name, age) values ('Sue', 12);
+      insert into students (name, age) values ('Tim', 13);
+      select name from students order by age asc;
+    EOS
+
+    names = shell_output("#{bin}/sqlite3 < #{path}").strip.split("\n")
+    assert_equal %w[Sue Tim Bob], names
+  end
+end
