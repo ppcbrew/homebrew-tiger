fc75d4c7f014afb68d996b0e790ae550b8ad7324
ppcbrew <macuser@imacg5>
2020-05-31 05:30:55 -0500

    adding dummy formula to dump cflags

---
+++ Formula/cflags.rb
@@ -0,0 +1,12 @@
+class Cflags < Formula
+  revision 100
+  desc "Real-time data compression library"
+  homepage "http://www.oberhumer.com/opensource/lzo/"
+  url "http://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz"
+  sha1 "4924676a9bae5db58ef129dc1cebce3baa3c4b5d"
+
+  def install
+    system "env"
+    system "false"
+  end
+end
