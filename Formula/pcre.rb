9c16c53d2fb52b7f2b98d86a4855fcfe598396b5
ppcbrew <macuser@imacg5>
2020-05-30 19:39:18 -0500

    pulling in pcre

--- Formula/pcre.rb
+++ Formula/pcre.rb
@@ -1,56 +1,45 @@
 class Pcre < Formula
   revision 100
   desc "Perl compatible regular expressions library"
-  homepage "http://www.pcre.org/"
-  url "https://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.39.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/downloads.sourceforge.net/p/pc/pcre/pcre/8.39/pcre-8.39.tar.bz2"
-  sha256 "b858099f82483031ee02092711689e7245586ada49e534a06e678b8ea9549e8b"
+  homepage "https://www.pcre.org/"
+  url "https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.exim.org/pub/pcre/pcre-8.44.tar.bz2"
+  sha256 "19108658b23b3ec5058edc9f66ac545ea19f9537234be1ec62b714c84399366d"
 
   bottle do
     cellar :any
     root_url "https://f002.backblazeb2.com/file/bottles"
-    sha256 "d1622641fd2eb15d4d8f1f37b2e1f1bd3ad233ea7c2bab3b949a7f0e3036e2ba" => :tiger_g4e
   end
 
   head do
     url "svn://vcs.exim.org/pcre/code/trunk"
 
-    depends_on "automake" => :build
     depends_on "autoconf" => :build
+    depends_on "automake" => :build
     depends_on "libtool" => :build
   end
 
-  option :universal
-
-  # See https://github.com/mistydemeo/tigerbrew/issues/93
-  fails_with :gcc do
-    build 5553
-    cause "One test failure on G4"
-  end
-
-  fails_with :llvm do
-    build 2326
-    cause "Bus error in ld on SL 10.6.4"
-  end
+  #uses_from_macos "bzip2"
+  #uses_from_macos "zlib"
 
   def install
-    ENV.universal_binary if build.universal?
-
-    args = [ "--disable-dependency-tracking",
-              "--prefix=#{prefix}",
-              "--enable-utf8",
-              "--enable-pcre8",
-              "--enable-pcre16",
-              "--enable-pcre32",
-              "--enable-unicode-properties",
-              "--enable-pcregrep-libz",
-              "--enable-pcregrep-libbz2" ]
-
-    # JIT fails tests very badly on PPC right now
-    args << "--enable-jit" unless Hardware::CPU.type == :ppc
+    args = %W[
+      CFLAGS=#{RbConfig::CONFIG["CFLAGS"]}
+      ./configure
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --enable-utf8
+      --enable-pcre8
+      --enable-pcre16
+      --enable-pcre32
+      --enable-unicode-properties
+      --enable-pcregrep-libz
+      --enable-pcregrep-libbz2
+    ]
+    args << "--enable-jit" if MacOS.version >= :sierra
 
     system "./autogen.sh" if build.head?
-    system "./configure", *args
+    system "env", *args
     system "make"
     ENV.deparallelize
     system "make", "test"
