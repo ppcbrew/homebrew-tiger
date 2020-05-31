93f38d3b0a561a4e0f67fc1e4f606034e1e2a645
ppcbrew <macuser@ibookg3>
2020-05-30 19:42:13 -0500

    adding tiger_g3 bottle of lzop 1.04

--- Formula/lzop.rb
+++ Formula/lzop.rb
@@ -9,6 +9,7 @@ class Lzop < Formula
     cellar :any
     root_url "https://f002.backblazeb2.com/file/bottles"
     sha256 "393df3029b79f1ea67d8451568923fdc125224e1642dc57d6e3c9ef677dd2679" => :leopard_g5
+    sha256 "a77ea6ff9b8e80c1b932a0bb5697b0f234733df7cd78dc20ff93f35e88bc31c2" => :tiger_g3
     sha256 "cb9a2b8daa757420e11090c7ea8457dc098c722b7af50a665784f25fd2cd6e28" => :tiger_g4e
   end
 
