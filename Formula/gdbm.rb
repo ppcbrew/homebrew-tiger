5d72a1c9d01c04f037a0cd21037fef5c39c54fe9
ppcbrew <macuser@imacg5>
2020-05-31 01:23:44 -0500

    pulling in gdbm 1.18.1

---
+++ Formula/gdbm.rb
@@ -0,0 +1,36 @@
+class Gdbm < Formula
+  revision 100
+  desc "GNU database manager"
+  homepage "https://www.gnu.org/software/gdbm/"
+  url "https://ftp.gnu.org/gnu/gdbm/gdbm-1.18.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/gdbm/gdbm-1.18.1.tar.gz"
+  sha256 "86e613527e5dba544e73208f42b78b7c022d4fa5a6d5498bf18c8d6f745b91dc"
+
+  bottle do
+    cellar :any
+    root_url "https://f002.backblazeb2.com/file/bottles"
+  end
+
+  option "with-libgdbm-compat", "Build libgdbm_compat, a compatibility layer which provides UNIX-like dbm and ndbm interfaces."
+
+  def install
+    ENV.universal_binary if build.universal?
+
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+    ]
+
+    args << "--enable-libgdbm-compat" if build.with? "libgdbm-compat"
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    pipe_output("#{bin}/gdbmtool --norc --newdb test", "store 1 2\nquit\n")
+    assert File.exist?("test")
+    assert_match /2/, pipe_output("#{bin}/gdbmtool --norc test", "fetch 1\nquit\n")
+  end
+end
