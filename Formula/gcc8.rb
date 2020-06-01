2659fcabb8b529fea2398cb66d690cbcf05c628c
Jason Pepas <jasonpepas@gmail.com>
2020-05-31 20:38:16 -0500

    pulling in gcc 8

---
+++ Formula/gcc8.rb
@@ -0,0 +1,205 @@
+class Gcc8 < Formula
+  revision 100
+  desc "GNU compiler collection"
+  homepage "https://gcc.gnu.org/"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-8.4.0/gcc-8.4.0.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gcc/gcc-8.4.0/gcc-8.4.0.tar.xz"
+  sha256 "e30a6e52d10e1f27ed55104ad233c30bd1e99cfb5ff98ab022dc941edd1b2dd4"
+
+  bottle do
+    root_url "https://f002.backblazeb2.com/file/bottles"
+  end
+
+  # The bottles are built on systems with the CLT installed, and do not work
+  # out of the box on Xcode-only systems due to an incorrect sysroot.
+  def pour_bottle?
+    MacOS::CLT.installed?
+  end
+
+  depends_on "ppcbrew/tiger/gmp"
+  depends_on "ppcbrew/tiger/libmpc"
+  depends_on "ppcbrew/tiger/mpfr"
+  depends_on "ppcbrew/tiger/isl"
+
+  # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
+  cxxstdlib_check :skip
+
+  option "with-nls", "Build with native language support (localization)"
+  option "with-jit", "Build just-in-time compiler"
+  # enabling multilib on a host that can't run 64-bit results in build failures
+  option "without-multilib", "Build without multilib support" if MacOS.prefer_64_bit?
+
+  if MacOS.version < :leopard
+    # The as that comes with Tiger isn't capable of dealing with the
+    # PPC asm that comes in libitm
+    depends_on "cctools" => :build
+  end
+
+  fails_with :gcc_4_0
+  fails_with :llvm0
+
+  def arch
+    if Hardware::CPU.type == :intel
+      if MacOS.prefer_64_bit?
+        "x86_64"
+      else
+        "i686"
+      end
+    elsif Hardware::CPU.type == :ppc
+      if MacOS.prefer_64_bit?
+        "powerpc64"
+      else
+        "powerpc"
+      end
+    end
+  end
+
+  def install
+    # GCC will suffer build errors if forced to use a particular linker.
+    ENV.delete "LD"
+
+    version_suffix = version.to_s.slice(/\d/)
+
+    # Even when suffixes are appended, the info pages conflict when
+    # install-info is run so pretend we have an outdated makeinfo
+    # to prevent their build.
+    ENV["gcc_cv_prog_makeinfo_modern"] = "no"
+
+    # Otherwise libstdc++ will be incorrectly tagged with cpusubtype 10 (G4e)
+    # https://github.com/mistydemeo/tigerbrew/issues/538
+    if Hardware::CPU.family == :g3 || ARGV.bottle_arch == :g3
+      ENV.append_to_cflags "-force_cpusubtype_ALL"
+    end
+
+    if MacOS.version < :leopard
+      ENV["AS"] = ENV["AS_FOR_TARGET"] = "#{Formula["cctools"].bin}/as"
+    end
+
+    # We avoiding building:
+    #  - Ada, which requires a pre-existing GCC Ada compiler to bootstrap
+    #  - Go, currently not supported on macOS
+    #  - BRIG
+    languages = %w[c c++ objc obj-c++ fortran]
+
+    osmajor = `uname -r`.split(".").first
+    pkgversion = "Tigerbrew #{name} #{pkg_version} #{build.used_options*" "}".strip
+
+    # JIT compiler is off by default, enabling it has performance cost
+    languages << "jit" if build.with? "jit"
+
+    args = %W[
+      --build=#{arch}-apple-darwin#{osmajor},
+      --prefix=#{prefix},
+      --libdir=#{lib}/gcc/#{version_suffix},
+      --enable-checking=release,
+      --enable-languages=#{languages.join(",")},
+      --program-suffix=-#{version_suffix},
+      --with-gmp=#{Formula["gmp"].opt_prefix},
+      --with-mpfr=#{Formula["mpfr"].opt_prefix},
+      --with-mpc=#{Formula["libmpc"].opt_prefix},
+      --with-isl=#{Formula["isl"].opt_prefix},
+      --with-system-zlib,
+      --with-pkgversion=#{pkgversion},
+      --with-bugurl=https://github.com/mistydemeo/tigerbrew/issues,
+    ]
+
+    if build.without?("multilib") || !MacOS.prefer_64_bit?
+      args << "--disable-multilib"
+    else
+      args << "--enable-multilib"
+    end
+
+    unless MacOS::CLT.installed?
+      # For Xcode-only systems, we need to tell the sysroot path.
+      # "native-system-headers" will be appended
+      args << "--with-native-system-header-dir=/usr/include"
+      args << "--with-sysroot=#{MacOS.sdk_path}"
+    end
+
+    # Ensure correct install names when linking against libgcc_s;
+    # see discussion in https://github.com/Homebrew/homebrew/pull/34303
+    inreplace "libgcc/config/t-slibgcc-darwin", "@shlib_slibdir@", "#{HOMEBREW_PREFIX}/lib/gcc/#{version_suffix}"
+
+    # "Building GCC with plugin support requires a host that supports
+    # -fPIC, -shared, -ldl and -rdynamic."
+    args << "--enable-plugin" if MacOS.version > :leopard
+
+    # Otherwise make fails during comparison at stage 3
+    # See: http://gcc.gnu.org/bugzilla/show_bug.cgi?id=45248
+    args << "--with-dwarf2" if MacOS.version < :leopard
+
+    args << "--disable-nls" if build.without? "nls"
+
+    args << "--enable-host-shared" if build.with?("jit")
+
+    mkdir "build" do
+
+      system "../configure", *args
+      system "make"
+      system "make", "install"
+
+      bin.install_symlink bin/"gfortran-#{version_suffix}" => "gfortran"
+    end
+
+    # Handle conflicts between GCC formulae and avoid interfering
+    # with system compilers.
+    # Since GCC 4.8 libffi stuff are no longer shipped.
+    # Rename man7.
+    Dir.glob(man7/"*.7") { |file| add_suffix file, version_suffix }
+  end
+
+  def add_suffix(file, suffix)
+    dir = File.dirname(file)
+    ext = File.extname(file)
+    base = File.basename(file, ext)
+    File.rename file, "#{dir}/#{base}-#{suffix}#{ext}"
+  end
+
+  def caveats
+    if build.with?("multilib") then <<-EOS.undent
+      GCC has been built with multilib support. Notably, OpenMP may not work:
+        https://gcc.gnu.org/bugzilla/show_bug.cgi?id=60670
+      If you need OpenMP support you may want to
+        brew reinstall gcc --without-multilib
+      EOS
+    end
+  end
+
+  test do
+    (testpath/"hello-c.c").write <<-EOS.undent
+      #include <stdio.h>
+      int main()
+      {
+        puts("Hello, world!");
+        return 0;
+      }
+    EOS
+    system "#{bin}/gcc-#{version_suffix}", "-o", "hello-c", "hello-c.c"
+    assert_equal "Hello, world!\n", `./hello-c`
+
+    (testpath/"hello-cc.cc").write <<-EOS.undent
+      #include <iostream>
+      int main()
+      {
+        std::cout << "Hello, world!" << std::endl;
+        return 0;
+      }
+    EOS
+    system "#{bin}/g++-#{version_suffix}", "-o", "hello-cc", "hello-cc.cc"
+    assert_equal "Hello, world!\n", `./hello-cc`
+
+    (testpath/"test.f90").write <<-EOS.undent
+      integer,parameter::m=10000
+      real::a(m), b(m)
+      real::fact=0.5
+
+      do concurrent (i=1:m)
+        a(i) = a(i) + fact*b(i)
+      end do
+      write(*,"(A)") "Done"
+      end
+    EOS
+    system "#{bin}/gfortran", "-o", "test", "test.f90"
+    assert_equal "Done\n", `./test`
+  end
+end
